5b0004c245691c49952df48abee8e8dd67abf292
LewisDchn <lewis.duchesne@outlook.fr>
2020-06-14 21:53:40 +0200

    debut du cv v2

---
+++ assets/js/typeIt.js
@@ -0,0 +1,12 @@
+new TypeIt('#subtitle', {
+    speed: 50
+  })
+  .pause(3000)
+  .type('Bienvenue sur mon CV Numérique')
+  .pause(1300)
+  .options({speed: 200})
+  .delete(20)
+  .options({speed: 50})
+  .pause(500)
+  .type('dans mon univers.')
+  .go();
